82ba4dd546ab597add4f186eacaec33b69a0dc7a
thaohuynh <thao.huynh@c0x12c.com>
2024-11-08 04:59:09 +0700

    aws/acm: add module acm.

---
+++ aws/acm/main.tf
@@ -0,0 +1,44 @@
+resource "aws_acm_certificate" "this" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  subject_alternative_names = var.subject_alternative_names
+
+  tags = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+// create a route53 record for each domain validation option to validate the domain
+resource "aws_route53_record" "validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.this.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id         = var.zone_id
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+
+  depends_on = [aws_acm_certificate.this]
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  count = var.wait_for_validation ? 1 : 0
+
+  certificate_arn = aws_acm_certificate.this[0].arn
+
+  validation_record_fqdns = flatten([aws_route53_record.validation[*].fqdn])
+
+  timeouts {
+    create = var.validation_timeout
+  }
+}
---
+++ aws/acm/variables.tf
@@ -0,0 +1,35 @@
+variable "domain_name" {
+  description = "A domain name for which the certificate should be issued"
+  type        = string
+  default     = ""
+}
+
+variable "subject_alternative_names" {
+  description = "A list of domains that should be SANs in the issued certificate"
+  type        = list(string)
+  default     = []
+}
+
+variable "zone_id" {
+  description = "The ID of the hosted zone to contain this record. Required when validating via Route53"
+  type        = string
+  default     = ""
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "wait_for_validation" {
+  description = "Whether to wait for the validation to complete"
+  type        = bool
+  default     = true
+}
+
+variable "validation_timeout" {
+  description = "Define maximum timeout to wait for the validation to complete"
+  type        = string
+  default     = null
+}
