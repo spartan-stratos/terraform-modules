37509b9ea0cc067b85ffb20abf198e95f9c52aa7
thaohuynh <thao.huynh@c0x12c.com>
2024-11-11 21:57:42 +0700

    aws/ecs-application: add module ecs-application.

---
+++ aws/ecs-application/security_group.tf
@@ -0,0 +1,30 @@
+/*
+aws_security_group provides a security group resource for ECS service.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group.html
+*/
+resource "aws_security_group" "main" {
+  vpc_id      = var.vpc_id
+  name        = "${var.name}-sg"
+  description = "Allow inbound HTTP traffic to service from ALB only"
+
+
+  ingress {
+    description     = "allow from ALB only"
+    from_port       = var.container_port
+    to_port         = var.container_port
+    protocol        = "tcp"
+    security_groups = var.alb_security_groups
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
