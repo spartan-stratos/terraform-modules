56b570e09e956505b5c21048aba1241deccf9db0
thaohuynh <thao.huynh@c0x12c.com>
2024-11-11 23:09:51 +0700

    aws/vpn: add comments.

--- aws/vpn/main.tf
+++ aws/vpn/main.tf
@@ -1,9 +1,17 @@
+/*
+tls_private_key creates a PEM (and OpenSSH) formatted private key.
+https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/private_key
+*/
 resource "tls_private_key" "this" {
   count     = local.create_self_signed_cert ? 1 : 0
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
+/*
+tls_self_signed_cert creates a self-signed TLS certificate in PEM (RFC 1421) format.
+https://registry.terraform.io/providers/hashicorp/tls/latest/docs/resources/self_signed_cert
+*/
 resource "tls_self_signed_cert" "this" {
   count           = local.create_self_signed_cert ? 1 : 0
   private_key_pem = tls_private_key.this[0].private_key_pem
@@ -22,6 +30,10 @@ resource "tls_self_signed_cert" "this" {
   ]
 }
 
+/*
+aws_acm_certificate allows requesting and management of certificates from the Amazon Certificate Manager.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/acm_certificate
+*/
 resource "aws_acm_certificate" "this" {
   count            = local.create_self_signed_cert ? 1 : 0
   private_key      = tls_private_key.this[0].private_key_pem
@@ -34,21 +46,37 @@ resource "aws_acm_certificate" "this" {
   )
 }
 
+/*
+aws_vpc provides details about a specific VPC.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/vpc
+*/
 data "aws_vpc" "this" {
   id = var.endpoint_vpc_id
 }
 
+/*
+aws_cloudwatch_log_group manages an AWS CloudWatch log group.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group
+*/
 resource "aws_cloudwatch_log_group" "this" {
   name              = "${var.cloudwatch_log_group_name_prefix}${var.endpoint_name}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
   tags              = var.tags
 }
 
+/*
+aws_cloudwatch_log_stream creates a log stream within a CloudWatch log group.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_stream
+*/
 resource "aws_cloudwatch_log_stream" "this" {
   log_group_name = aws_cloudwatch_log_group.this.name
   name           = "connection-log"
 }
 
+/*
+aws_security_group defines a security group with egress rules in AWS.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+*/
 resource "aws_security_group" "this" {
   name        = "client-vpn-endpoint-${var.endpoint_name}"
   description = "Egress All. Used for other groups where VPN access is required. "
@@ -62,6 +90,10 @@ resource "aws_security_group" "this" {
   tags = var.tags
 }
 
+/*
+aws_ec2_client_vpn_endpoint creates a VPN endpoint in AWS for secure client access.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_client_vpn_endpoint
+*/
 resource "aws_ec2_client_vpn_endpoint" "this_sso" {
   count                  = var.create_endpoint ? 1 : 0
   description            = var.endpoint_name
@@ -91,12 +123,20 @@ resource "aws_ec2_client_vpn_endpoint" "this_sso" {
   )
 }
 
+/*
+aws_ec2_client_vpn_network_association associates a VPN endpoint with a VPC subnet.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_client_vpn_network_association
+*/
 resource "aws_ec2_client_vpn_network_association" "this_sso" {
   for_each               = var.create_endpoint ? toset(var.endpoint_subnets) : toset([])
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this_sso[0].id
   subnet_id              = each.value
 }
 
+/*
+aws_ec2_client_vpn_authorization_rule allows access to specific network CIDRs for client VPN.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_client_vpn_authorization_rule
+*/
 resource "aws_ec2_client_vpn_authorization_rule" "this_sso_to_dns" {
   count                  = var.create_endpoint && var.use_vpc_internal_dns ? 1 : 0
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this_sso[0].id
@@ -105,6 +145,10 @@ resource "aws_ec2_client_vpn_authorization_rule" "this_sso_to_dns" {
   description            = "Authorization for ${var.endpoint_name} to DNS"
 }
 
+/*
+aws_ec2_client_vpn_authorization_rule configures access rules for each specified CIDR and access group.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_client_vpn_authorization_rule
+*/
 resource "aws_ec2_client_vpn_authorization_rule" "this" {
   for_each               = var.create_endpoint ? var.authorization_rules : {}
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this_sso[0].id
@@ -113,6 +157,10 @@ resource "aws_ec2_client_vpn_authorization_rule" "this" {
   description            = "Rule name: ${each.key}"
 }
 
+/*
+aws_ec2_client_vpn_authorization_rule applies authorization to all groups for specified CIDRs.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_client_vpn_authorization_rule
+*/
 resource "aws_ec2_client_vpn_authorization_rule" "this_all_groups" {
   for_each               = var.create_endpoint ? var.authorization_rules_all_groups : {}
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this_sso[0].id
@@ -121,6 +169,10 @@ resource "aws_ec2_client_vpn_authorization_rule" "this_all_groups" {
   description            = "Rule name: ${each.key}"
 }
 
+/*
+aws_ec2_client_vpn_route sets routes for VPN connections to reach additional destination CIDRs.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_client_vpn_route
+*/
 resource "aws_ec2_client_vpn_route" "this_sso" {
   for_each               = var.create_endpoint ? { for r in var.additional_routes : "${r.subnet_id}:${r.destination_cidr_block}" => r } : {}
   client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.this_sso[0].id
