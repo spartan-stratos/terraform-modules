1df4e210fdc0a107abf71c555a8fb29512bf1189
thaohuynh <thao.huynh@c0x12c.com>
2024-11-11 22:43:43 +0700

    aws/vpn: add module vpn.

---
+++ aws/vpn/examples/complete/outputs.tf
@@ -0,0 +1,7 @@
+output "ec2_client_vpn_endpoint_id" {
+  value = module.vpn.ec2_client_vpn_endpoint_id
+}
+
+output "ec2_client_vpn_endpoint_arn" {
+  value = module.vpn.ec2_client_vpn_endpoint_arn
+}
