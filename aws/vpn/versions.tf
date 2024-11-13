beed4714a3b02dda844891eedc155c0a8158472c
thaohuynh <thao.huynh@c0x12c.com>
2024-11-13 09:06:05 +0700

    aws/ecs-application: update versions.tf

--- aws/vpn/versions.tf
+++ aws/vpn/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.40"
+      version = ">= 5.75"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 3.2.0"
+      version = ">= 4.0"
     }
   }
 }
