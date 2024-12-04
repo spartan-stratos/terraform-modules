92c2ec75f357eb8556b8400a03d72222f1386da3
Toan Nhan <toan.nhan@c0x12c.com>
2024-12-04 17:17:16 +0700

    feat: update static-website module

--- aws/static-website/variables.tf
+++ aws/static-website/variables.tf
@@ -23,11 +23,6 @@ variable "route53_zone_id" {
   type        = string
 }
 
-variable "route53_zone_name" {
-  description = "R53 zone name"
-  type        = string
-}
-
 variable "viewer_protocol_policy" {
   description = "Determines the protocols that viewers can use to access your CloudFront distribution."
   type        = string
