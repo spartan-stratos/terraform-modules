05a84d8156d9dccaef6c5ba63a61093d03364591
spartan-haopham <hao.pham@c0x12c.com>
2024-12-04 10:40:51 +0700

    Reformat files, update CHANGELOG and terraform version

--- aws/ses/main.tf
+++ aws/ses/main.tf
@@ -1,17 +1,27 @@
-# Create the SES domain identity for the specified email domain
+/*
+`aws_ses_domain_identity` creates an SES domain identity for the specified email domain.
+Used here to verify the ownership of the domain specified in `var.email_domain` for Amazon SES.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ses_domain_identity
+*/
 resource "aws_ses_domain_identity" "current" {
   domain = var.email_domain
 }
 
-# aws_ses_email_identity Provides an SES email identity resource
-
-
+/*
+`aws_ses_email_identity` `emails` provides an SES email identity resource for verifying email addresses in Amazon SES.
+Used here to create email identities for all emails listed in the `var.emails` variable.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ses_email_identity
+*/
 resource "aws_ses_email_identity" "emails" {
   count = length(var.emails)
   email = var.emails[count.index]
 }
 
-# Datasource to receive SES Full Access to the ses domain
+/*
+`aws_iam_policy_document` generates an IAM policy document to allow SES full access for the specified principals.
+Used here to define permissions for roles or entities in `var.principal_roles` to access the SES domain.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+*/
 data "aws_iam_policy_document" "this" {
   statement {
     effect = "Allow"
@@ -26,6 +36,11 @@ data "aws_iam_policy_document" "this" {
   }
 }
 
+/*
+`aws_iam_role_policy` attaches an IAM policy to the specified roles in `var.principal_roles`.
+Used here to grant the roles the permissions defined in the generated IAM policy document.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
+*/
 resource "aws_iam_role_policy" "this" {
   count  = length(var.principal_roles)
   role   = var.principal_roles[count.index]
--- aws/ses/variables.tf
+++ aws/ses/variables.tf
@@ -1,16 +1,16 @@
 variable "email_domain" {
-  type        = string
   description = "The domain name for which SES will be configured (e.g., 'example.com')."
+  type        = string
 }
 
 variable "emails" {
-  type        = list(string)
-  default     = []
   description = "List of email addresses allowed for sending in SES sandbox mode. These are verified to send emails during testing."
+  type        = list(string).0
+  default     = []
 }
 
 variable "principal_roles" {
+  description = "List of IAM roles that should have access to SES. These roles will have permissions to manage and use SES for the configured domain."
   type        = list(string)
   default     = []
-  description = "List of IAM roles that should have access to SES. These roles will have permissions to manage and use SES for the configured domain."
 }
