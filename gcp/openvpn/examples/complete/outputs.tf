cff15ae9316f29d9c2d75d07a211a9e69f74e51d
spartan-haopham <hao.pham@c0x12c.com>
2024-12-06 13:59:53 +0700

    update outputs

--- gcp/openvpn/examples/complete/outputs.tf
+++ gcp/openvpn/examples/complete/outputs.tf
@@ -1,13 +1,13 @@
 output "public_ip" {
-  value = module.public_ip
+  value = module.openvpn.public_ip
 }
 
 output "ca_cert" {
-  value = module.ca_cert
+  value = module.openvpn.ca_cert
 }
 
 output "ssh_public_key" {
-  value = module.ssh_public_key
+  value = module.openvpn.ssh_public_key
 }
 
 output "ovpn_file" {
