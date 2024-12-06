87bd649b30af78af31621317327c52ad2fc43dcd
Spartan - Y Truong <160025009+spartan-ytruong@users.noreply.github.com>
2024-12-06 09:35:40 +0700

    Init GCP service config (#39)

---
+++ gcp/service-config/examples/complete/README.md
@@ -0,0 +1,9 @@
+# Service config
+
+## Usage
+To run this example you need to execute:
+```bash
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
---
+++ gcp/service-config/examples/complete/outputs.tf
@@ -0,0 +1,7 @@
+output "service_account_name" {
+  value = module.service.service_account_name
+}
+
+output "service_account_email" {
+  value = module.service.service_account_email
+}
