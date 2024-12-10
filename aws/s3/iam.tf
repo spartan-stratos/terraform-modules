142902ddaad72506b0a55598e0f415c267d94bf7
spartan-haopham <hao.pham@c0x12c.com>
2024-12-10 13:00:33 +0700

    Add policy for s3

--- aws/s3/iam.tf
+++ aws/s3/iam.tf
@@ -55,3 +55,37 @@ resource "aws_iam_policy" "readonly" {
 
   policy = data.aws_iam_policy_document.readonly_policy.0.json
 }
+
+
+/*
+aws_iam_policy read_write_policy creates an IAM policy for read_write_policy access to the specified S3 bucket.
+The policy allows listing, getting objects, and viewing object ACLs within the bucket.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+*/
+data "aws_iam_policy_document" "read_write_policy" {
+  count = var.enabled_iam_policy ? 1 : 0
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:DeleteObject",
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:ListBucket",
+      "s3:ObjectOwnerOverrideToBucketOwner",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ]
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*"
+    ]
+  }
+}
+
+# Create the read-write IAM policy
+resource "aws_iam_policy" "read_write" {
+  count       = var.enabled_iam_policy ? 1 : 0
+  name        = "S3ReadWrite-${aws_s3_bucket.bucket.bucket}"
+  description = "Policy that allows writing to the S3 bucket"
+  policy      = data.aws_iam_policy_document.read_write_policy.json
+}
