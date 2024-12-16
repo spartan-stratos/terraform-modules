467bbc567a45a70405ae941e157797b1aa118140
spartan-ytruong <y.truong@c0x12c.com>
2024-12-17 04:07:11 +0700

    update

--- aws/postgres/main.tf
+++ aws/postgres/main.tf
@@ -35,7 +35,7 @@ resource "aws_vpc_security_group_egress_rule" "this" {
 
 module "main_db_instance" {
   source                       = "./db_instance"
-  identifier                   = local.identifier
+  identifier                   = try(var.db_identifier, local.identifier)
   instance_class               = var.instance_class
   allocated_storage            = var.disk_size
   max_allocated_storage        = var.max_allocated_storage
@@ -63,7 +63,7 @@ module "main_db_instance" {
 module "replica_db_instance" {
   source                       = "./db_instance"
   count                        = var.replica_count
-  identifier                   = "${local.identifier}-replica-${count.index}"
+  identifier                   = "${try(var.db_identifier, local.identifier)}-replica-${count.index}"
   instance_class               = var.instance_class
   allocated_storage            = var.disk_size
   max_allocated_storage        = var.max_allocated_storage
