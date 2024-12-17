0855c1ce98159fb5ba989a0677fac247bf44be3d
spartan-ytruong <y.truong@c0x12c.com>
2024-12-18 05:46:56 +0700

    correct aws_security_group.allow_all_within_vpc ingress rule

--- aws/vpc/security-group.tf
+++ aws/vpc/security-group.tf
@@ -39,21 +39,22 @@ resource "aws_security_group" "allow_all_within_vpc" {
     from_port        = "0"
     to_port          = "0"
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks      = [aws_vpc.this.cidr_block]
     ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
-    from_port = 0
-    to_port   = 0
+    from_port = "0"
+    to_port   = "0"
     protocol  = "-1"
     self      = true
   }
 
   egress {
-    from_port   = "0"
-    to_port     = "0"
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = "0"
+    to_port          = "0"
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
