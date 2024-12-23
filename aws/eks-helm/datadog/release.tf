ae082db3f50f7a381154f62a2090b633b798506c
taind <tai.nguyen@c0x12c.com>
2024-12-23 20:20:40 +0700

    add datadog module

---
+++ aws/eks-helm/datadog/release.tf
@@ -0,0 +1,47 @@
+resource "random_password" "cluster_agent_token" {
+  length  = 32
+  special = false
+}
+
+resource "helm_release" "this" {
+  name             = var.helm_release_name
+  namespace        = var.namespace
+  repository       = "https://helm.datadoghq.com"
+  chart            = "datadog"
+  version          = var.chart_version
+  create_namespace = true
+  timeout          = var.timeout
+
+  set_sensitive {
+    name  = "datadog.apiKey"
+    value = var.datadog_api_key
+  }
+  set_sensitive {
+    name  = "datadog.appKey"
+    value = var.datadog_app_key
+  }
+  set_sensitive {
+    name  = "datadog.site"
+    value = var.datadog_site
+  }
+  set_sensitive {
+    name  = "datadog.clusterName"
+    value = var.cluster_name
+  }
+  set_sensitive {
+    name  = "clusterAgent.token"
+    value = random_password.cluster_agent_token.result
+  }
+
+  values = [
+    templatefile("${path.module}/values.yaml.tftpl", {
+      http_check_url : var.http_check_urls,
+      environment : var.environment
+    })
+  ]
+  lifecycle {
+    ignore_changes = [
+      timeout
+    ]
+  }
+}
---
+++ aws/eks-helm/datadog/variables.tf
@@ -0,0 +1,51 @@
+variable "namespace" {
+  description = "The Namespace of the services."
+  type        = string
+  default     = "datadog"
+}
+
+variable "environment" {
+  description = "Environment where the resources will be created."
+  type        = string
+}
+
+variable "helm_release_name" {
+  description = "The Helm release of the services."
+  type        = string
+  default     = "datadog"
+}
+
+variable "datadog_api_key" {
+  description = "The datadog api key"
+  type        = string
+}
+
+variable "datadog_app_key" {
+  description = "The datadog app key"
+  type        = string
+}
+
+variable "datadog_site" {
+  description = "The datadog site"
+  type        = string
+}
+
+variable "http_check_urls" {
+  description = "The list of urls for http check"
+  type        = list(string)
+  default     = []
+}
+
+variable "timeout" {
+  description = "Default timeout of datadog"
+  default     = 1200
+}
+
+variable "cluster_name" {
+  description = "The EKS cluster name"
+}
+
+variable "chart_version" {
+  type    = string
+  default = "3.65.1"
+}
