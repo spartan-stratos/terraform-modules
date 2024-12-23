efe6a6c6dbd1937a287b5fa32dcc166df59c102f
spartan-haopham <hao.pham@c0x12c.com>
2024-12-23 11:10:45 +0700

    Update KMS policy

--- aws/kms/iam.tf
+++ aws/kms/iam.tf
@@ -1,6 +1,16 @@
 data "aws_iam_policy_document" "this" {
   statement {
     effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = "*"
+    }
+    actions   = "kms:*"
+    resources = "*"
+  }
+
+  statement {
+    effect = "Allow"
 
     principals {
       type        = "AWS"
