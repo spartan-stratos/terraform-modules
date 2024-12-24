c1eadd14768adab66f2d37fcd23dcff6e8a9434b
Spartan - Tai Nguyen <tai.nguyen@c0x12c.com>
2024-12-24 14:09:56 +0700

    Add eks-helm modules - Metrics-server (#93)

---
+++ aws/eks-helm/metrics-server/examples/main.tf
@@ -0,0 +1,7 @@
+module "example" {
+  source = "../"
+
+  helm_release_name  = "metrics-server"
+  namespace          = "kube-system"
+  helm_chart_version = "3.12.2"
+}
---
+++ aws/eks-helm/metrics-server/variables.tf
@@ -0,0 +1,16 @@
+variable "namespace" {
+  description = "The Namespace of the services."
+  type        = string
+  default     = "kube-system"
+}
+variable "helm_release_name" {
+  description = "The Helm release of the services."
+  type        = string
+  default     = "metrics-server"
+}
+
+variable "helm_chart_version" {
+  default     = "3.12.2"
+  type        = string
+  description = "The chart version of ingress controller"
+}
