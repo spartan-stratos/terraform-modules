78483ffc13bcc603a2d62da64a57159b9f120af3
spartan-haopham <hao.pham@c0x12c.com>
2024-12-24 15:50:57 +0700

    Update policy

---
+++ aws/ses/iam.tf
@@ -0,0 +1,61 @@
+/*
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+*/
+data "aws_iam_policy_document" "identity_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = var.principal_roles != null ? var.principal_roles : ["*"]
+    }
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail"
+    ]
+    resources = [aws_ses_domain_identity.this.arn]
+  }
+}
+
+/*
+`aws_ses_identity_policy` attaches an IAM policy to an SES identity.
+Used here to define and attach permissions to the domain identity specified in `aws_ses_domain_identity.current.arn`.
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ses_identity_policy
+*/
+resource "aws_ses_identity_policy" "this" {
+  identity = aws_ses_domain_identity.this.arn
+  name     = "SES"
+  policy   = data.aws_iam_policy_document.identity_policy.json
+}
+
+/*
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document
+ */
+data "aws_iam_policy_document" "this" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail"
+    ]
+    resources = [aws_ses_domain_identity.this.arn]
+  }
+}
+
+/*
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy
+ */
+resource "aws_iam_role_policy" "this" {
+  count  = length(var.iam_role_ids)
+  role   = var.iam_role_ids[count.index]
+  policy = data.aws_iam_policy_document.this.json
+}
+
+/*
+https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
+*/
+resource "aws_iam_policy" "this" {
+  name        = "SendEmail-${aws_ses_domain_identity.this.id}"
+  description = "Policy that allows request SES to send email"
+  policy      = data.aws_iam_policy_document.this.json
+}
--- aws/ses/outputs.tf
+++ aws/ses/outputs.tf
@@ -2,3 +2,7 @@ output "domain_identity_arn" {
   value       = aws_ses_domain_identity.this.arn
   description = "The ARN of the SES domain identity."
 }
+
+output "iam_policy_ses_send_email" {
+  value = aws_iam_policy.this.arn
+}
