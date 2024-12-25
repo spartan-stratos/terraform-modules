29a7694a50858765d659d356ac46cc4aaf2eae72
taind <tai.nguyen@c0x12c.com>
2024-12-25 11:44:13 +0700

    correct example

--- aws/eks-service/examples/main.tf
+++ aws/eks-service/examples/main.tf
@@ -5,8 +5,18 @@ module "eks_service" {
   eks_oidc_provider = {
     arn = "arn:aws:iam::123456789012:oidc-provider/my-eks-cluster-oidc-provider"
   }
-  alb_dns         = "my-alb-dns"
-  service         = "my-service"
+  alb_dns = "my-alb-dns"
+  service = {
+    name      = "my-service"
+    namespace = "my-namespace"
+    hostnames = ["my-service.example.com"]
+    config_map = {
+      "HELLO" = "WORLD"
+    }
+    secrets = {
+      "SECRET" = "super-secret"
+    }
+  }
   route53_zone_id = "my-route53-zone-id"
   region          = "us-west-2"
 }
