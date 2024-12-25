ce3190078fedfd1d2d4417a773e917d65dbae1cd
spartan-haopham <hao.pham@c0x12c.com>
2024-12-25 10:54:45 +0700

    fix relative templates path

--- aws/amplify/locals.tf
+++ aws/amplify/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  build_spec  = var.enable_backend ? "${path.module}/build_spec_with_backend.tftpl" : "${path.module}/build_spec_frontend_only.tftpl"
+  build_spec  = var.enable_backend ? "${path.module}/templates/build_spec_with_backend.tftpl" : "${path.module}/templates/build_spec_frontend_only.tftpl"
   domain_name = var.sub_domain == "" ? var.dns_zone : "${var.sub_domain}.${var.dns_zone}"
 
   custom_redirect_rules = var.enable_redirect_to_root ? concat(
