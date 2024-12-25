bdccacdb61005b9bde6373eda72a4b82f46b0d54
Spartan - Y Truong <y.truong@c0x12c.com>
2024-12-25 09:07:22 +0700

    Add Datadog AWS integration and Datadog GCP integration modules (#98)

---
+++ datadog/aws-integration/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.75"
+    }
+
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "~> 3.46.0"
+    }
+  }
+}
