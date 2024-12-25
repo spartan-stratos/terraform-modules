ea345ef8a0242ab9601949f7f4bd7e8136090b22
spartan-haopham <hao.pham@c0x12c.com>
2024-12-25 11:43:54 +0700

    Update Amplify to be more general (#102)

--- aws/amplify/outputs.tf
+++ aws/amplify/outputs.tf
@@ -10,15 +10,24 @@ output "arn" {
 
 output "backend_environment_name" {
   description = "Created backend environment name"
-  value       = aws_amplify_backend_environment.this.environment_name
+  value       = var.enable_backend ? aws_amplify_backend_environment.this[0].environment_name : null
 }
 
 output "backend_environment_arn" {
   description = "Created backend environment arn"
-  value       = aws_amplify_backend_environment.this.arn
+  value       = var.enable_backend ? aws_amplify_backend_environment.this[0].arn : null
 }
 
-output "role_policy_name" {
-  description = "Created role policy name"
-  value       = aws_iam_role_policy.amplify_backend.name
+output "amplify_app_backend_role_name" {
+  description = "Created app backend role name"
+  value       = aws_iam_role.amplify_backend.name
+}
+
+output "amplify_app_backend_role_arn" {
+  description = "Created app backend role arn"
+  value       = aws_iam_role.amplify_backend.arn
+}
+output "domain_name" {
+  description = "Created domain name"
+  value       = aws_amplify_domain_association.this.domain_name
 }
