7044cd1ede347d19ca8f5085f81bcd9f2b7ee367
taind <tai.nguyen@c0x12c.com>
2024-12-25 10:05:55 +0700

    remove region in example

--- aws/cloudwatch/vpc-flow-logs/examples/main.tf
+++ aws/cloudwatch/vpc-flow-logs/examples/main.tf
@@ -4,6 +4,5 @@ module "vpc_flow_logs" {
   vpc_id            = "vpc-12345678"
   group_name        = "spatan-stratos"
   traffic_type      = "ALL"
-  region            = "us-west-2"
   retention_in_days = 10
 }
