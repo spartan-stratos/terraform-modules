52e6e593546cde61badbefb9eb4dfc5a6e5cc522
spartan-haopham <hao.pham@c0x12c.com>
2024-12-26 15:05:57 +0700

    Update service_hostnames output for eks_service

--- aws/eks-service/outputs.tf
+++ aws/eks-service/outputs.tf
@@ -2,3 +2,10 @@ output "pod_role" {
   value       = aws_iam_role.this.arn
   description = "The ARN of the IAM role for services' pods"
 }
+
+output "service_hostnames" {
+  description = "This output generates a map of service hostnames by iterating over the Route 53 records and extracting their fully qualified domain names (FQDNs). Each key corresponds to a unique identifier, and its value is the associated FQDN."
+  value = {
+    for k, hostname in aws_route53_record.service_hostnames : k => hostname.fqdn
+  }
+}
