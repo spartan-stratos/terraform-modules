727941a4922704e7dae98c96133af535ccad37f0
spartan-haopham <hao.pham@c0x12c.com>
2024-12-27 02:09:49 +0700

    add custom set variable

--- aws/eks-helm/metrics-server/variables.tf
+++ aws/eks-helm/metrics-server/variables.tf
@@ -15,13 +15,34 @@ variable "helm_chart_version" {
   description = "The chart version of ingress controller"
 }
 
-variable "set_configs" {
-  description = "To specify the list of set configs"
-  type = list(any)
-  default = [{
+variable "set_metrics_enabled" {
+  description = "To allow unauthenticated access to /metrics if value is true"
+  type = object({
+    name = string
+    value = bool
+  })
+  default = {
     name  = "metrics.enabled"
     value = false
-  }]
+  }
+}
+
+variable "set_container_port" {
+  description = "To specify the list of set configs"
+  type = object({
+    name = string
+    value = number
+  })
+  default = null
+}
+
+variable "set_rbac_create" {
+  description = "value"
+  type = object({
+    name = string
+    value = number
+  })
+  default = null
 }
 
 variable "set_list_config" {
