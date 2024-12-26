217b66a131c39205e566eed79f297a3d21f711be
spartan-haopham <hao.pham@c0x12c.com>
2024-12-26 15:13:55 +0700

    fix typo

--- aws/eks-service/outputs.tf
+++ aws/eks-service/outputs.tf
@@ -6,6 +6,6 @@ output "pod_role" {
 output "service_hostnames" {
   description = "This output generates a map of service hostnames by iterating over the Route 53 records and extracting their fully qualified domain names (FQDNs). Each key corresponds to a unique identifier, and its value is the associated FQDN."
   value = {
-    for k, hostname in aws_route53_record.service_hostnames : k => hostname.fqdn
+    for k, hostname in aws_route53_record.this : k => hostname.fqdn
   }
 }
