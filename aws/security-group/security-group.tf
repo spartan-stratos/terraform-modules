85dc9f9dc9b075f914c15ba6066b7747332b03a6
spartan-longnguyen <long.nguyen@c0x12c.com>
2024-12-30 01:30:05 +0700

    update

--- aws/security-group/security-group.tf
+++ aws/security-group/security-group.tf
@@ -17,15 +17,11 @@ resource "aws_vpc_security_group_ingress_rule" "test" {
 
   security_group_id = each.value.security_group_id
 
-  dynamic "ingress_rule" {
-    for_each = each.value.rules
-    content {
-      from_port   = ingress_rule.value.from_port
-      to_port     = ingress_rule.value.to_port
-      ip_protocol = ingress_rule.value.protocol
-      cidr_ipv4   = lookup(ingress_rule.value, "cidr_blocks", [])
-      cidr_ipv6   = lookup(ingress_rule.value, "ipv6_cidr_blocks", [])
-      description = lookup(ingress_rule.value, "description", "")
-    }
-  }
+  # Iterate over ingress rules directly
+  from_port   = each.value.rules.from_port
+  to_port     = each.value.rules.to_port
+  ip_protocol = each.value.rules.protocol
+  cidr_ipv4   = lookup(each.value.rules, "cidr_blocks", [])
+  cidr_ipv6   = lookup(each.value.rules, "ipv6_cidr_blocks", [])
+  description = lookup(each.value.rules, "description", "")
 }
