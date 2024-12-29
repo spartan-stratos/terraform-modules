5cb8b454f8adc3c6edaa129da33c0412027533bc
spartan-longnguyen <long.nguyen@c0x12c.com>
2024-12-30 01:53:59 +0700

    update

--- aws/security-group/security-group.tf
+++ aws/security-group/security-group.tf
@@ -7,21 +7,25 @@ resource "aws_security_group" "this" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "test" {
-  for_each = {
-    for sg in var.security_groups :
-    sg.name => {
-      security_group_id = aws_security_group.this[sg.name].id
-      rules             = sg.ingress_rules
-    }
-  }
+  for_each = flatten([
+    for sg in var.security_groups : [
+      for rule_key, rule in sg.ingress_rules : {
+        security_group_name = sg.name
+        from_port           = rule.from_port
+        to_port             = rule.to_port
+        ip_protocol         = rule.protocol
+        cidr_blocks         = rule.cidr_blocks
+        ipv6_cidr_blocks    = rule.ipv6_cidr_blocks
+        description         = rule.description
+      }
+    ]
+  ])
 
-  security_group_id = each.value.security_group_id
-
-  # Iterate over the rules map directly
-  from_port   = each.value.rules["from_port"]
-  to_port     = each.value.rules["to_port"]
-  ip_protocol = each.value.rules["protocol"]
-  cidr_ipv4   = lookup(each.value.rules, "cidr_blocks", [])
-  cidr_ipv6   = lookup(each.value.rules, "ipv6_cidr_blocks", [])
-  description = lookup(each.value.rules, "description", "")
+  security_group_id = aws_security_group.this[each.value.security_group_name].id
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  ip_protocol       = each.value.ip_protocol
+  cidr_ipv4         = each.value.cidr_blocks
+  cidr_ipv6         = each.value.ipv6_cidr_blocks
+  description       = each.value.description
 }
