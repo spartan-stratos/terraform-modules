761743e06336dc215c11e9994a08d41326a653b3
spartan-haopham <hao.pham@c0x12c.com>
2024-12-30 09:24:26 +0700

    add tags variables to vpc modules

--- aws/security-group/security-group.tf
+++ aws/security-group/security-group.tf
@@ -67,6 +67,7 @@ resource "aws_security_group" "this" {
   name        = each.value.name
   description = each.value.description
   vpc_id      = each.value.vpc_id
+  tags        = each.value.tags
 }
 
 
