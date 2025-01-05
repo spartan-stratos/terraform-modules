60cbfb02ccbb005af2146656e63e52c19f0462cb
spartan-ytruong <y.truong@c0x12c.com>
2025-01-05 20:03:56 +0700

    add examples and format

--- datadog/mono-monitor/examples/complete/main.tf
+++ datadog/mono-monitor/examples/complete/main.tf
@@ -8,4 +8,19 @@ module "mono_monitor" {
 
   pod_monitor_enabled = true
   http_check_enabled  = true
+
+  override_default_monitors = {
+    http_check = {
+      priority_level = 2
+      title_tags     = "[HTTP Check]"
+      title          = "URLs are not healthy"
+
+      type           = "service check"
+      query_template = "\"http.can_connect\".over(\"env:dev\").by(\"url\").last(2).count_by_status()"
+
+      threshold_critical = 1
+      ok                 = 1
+      renotify_interval  = 60
+    }
+  }
 }
--- datadog/service-monitor/examples/complete/main.tf
+++ datadog/service-monitor/examples/complete/main.tf
@@ -11,4 +11,22 @@ module "service_monitor" {
   cpu_monitor_enabled     = true
   memory_monitor_enabled  = true
   service_monitor_enabled = true
+
+  override_default_monitors = {
+    error_hit = {
+      priority_level = 3
+      title_tags     = "[High Error Hits]"
+      title          = "Service service-platform Error Hits is high"
+
+      query_template = "sum($${timeframe}):sum:$${metric}{env:dev,service:service-platform.as_count() > $${threshold_critical}"
+      query_args = {
+        timeframe = "last_5m"
+        metric    = "trace.fastapi.request.errors"
+      }
+
+      threshold_critical          = 1
+      threshold_critical_recovery = 0
+      renotify_interval           = 60
+    }
+  }
 }
