cc4f83a66193b31e130e0e7947fffcf227dd4b22
spartan-ytruong <y.truong@c0x12c.com>
2025-02-18 04:49:38 +0700

    add role arn to aws-auth configmap

--- aws/eks-cluster/aws-auth.tf
+++ aws/eks-cluster/aws-auth.tf
@@ -3,6 +3,13 @@ locals {
 
   aws_auth_configmap_data = {
     mapRoles = yamlencode(concat(
+      [
+        for node_group_name, node_group in var.node_groups : {
+          rolearn  = module.eks_managed_node_group[node_group_name].node_role_arn
+          username = "system:node:{{EC2PrivateDNSName}}"
+          groups   = ["system:bootstrappers", "system:nodes"]
+        }
+      ],
       [{
         rolearn  = aws_iam_role.node.arn
         username = "system:node:{{EC2PrivateDNSName}}"
@@ -10,8 +17,7 @@ locals {
           "system:bootstrappers",
           "system:nodes",
         ]
-        }
-      ]
+      }]
       ,
       [{
         rolearn  = aws_iam_role.auth_role.arn
@@ -19,8 +25,7 @@ locals {
         groups = [
           "system:masters",
         ]
-        }
-      ]
+      }]
       ,
       [{
         rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/jenkins-oidc-role"
@@ -28,8 +33,7 @@ locals {
         groups = [
           "system:masters",
         ]
-        }
-      ]
+      }]
       ,
       # Fargate profile
       [{
@@ -40,8 +44,7 @@ locals {
           "system:nodes",
           "system:node-proxier",
         ]
-        }
-      ]
+      }]
     ))
     mapUsers    = yamlencode(var.aws_auth_users)
     mapAccounts = yamlencode(var.aws_auth_accounts)
