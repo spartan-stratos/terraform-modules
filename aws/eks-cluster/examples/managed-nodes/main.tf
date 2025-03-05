c072786b3db3cac53ebd381ec02345b0444177a6
spartan-tainguyen <tai.nguyen@c0x12c.com>
2025-03-05 21:59:14 +0700

    fix examples

--- aws/eks-cluster/examples/managed-nodes/main.tf
+++ aws/eks-cluster/examples/managed-nodes/main.tf
@@ -34,7 +34,7 @@ module "eks" {
       desired_size    = "1"
       min_size        = "1"
       max_size        = "5"
-      taint           = []
+      taints          = []
     }
   }
   custom_namespaces = ["jenkins", "datadog", "service-bot"]
