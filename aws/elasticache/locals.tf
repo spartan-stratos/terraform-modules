0427c9b3fa86d03bdc5aa2aa3d370ade21109aec
spartan-toannhan <toan.nhan@c0x12c.com>
2025-04-16 18:02:08 +0700

    feat: custom redis paramters (#356)

---
+++ aws/elasticache/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  major_version        = split(".", var.engine_version)[0]
+  redis_family         = "redis${local.major_version}"
+  parameter_group_name = var.custom_redis_parameters == null ? var.parameter_group_name : aws_elasticache_parameter_group.this[0].name
+}
