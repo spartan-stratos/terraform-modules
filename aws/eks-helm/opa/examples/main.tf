f419390f23377636fd697c016b93ed43a891eaf1
spartan-nguyentruong <nguyen.truong@c0x12c.com>
2025-05-22 19:40:40 +0700

    Resolve comments

--- aws/eks-helm/opa/examples/main.tf
+++ aws/eks-helm/opa/examples/main.tf
@@ -1,7 +1,8 @@
 module "eks_helm_opa" {
-  source = "github.com/spartan-stratos/terraform-modules//aws/eks-helm/opa?ref=v0.3.0"
+  source = "github.com/spartan-stratos/terraform-modules//aws/eks-helm/opa?ref=v0.5.4"
 
   helm_release_name  = "opa"
-  namespace          = "spartan"
+  namespace          = "opa"
   helm_chart_version = "0.1.13"
+  opa_image_tag      = "1.4.2"
 }
