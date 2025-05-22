30510c516ed9e763b1a58ce69591a7fbb96149c4
spartan-nguyentruong <nguyen.truong@c0x12c.com>
2025-05-22 22:11:46 +0700

    run terraform fmt locally

--- aws/eks-helm/opa/main.tf
+++ aws/eks-helm/opa/main.tf
@@ -1,24 +1,24 @@
 ##### Open Policy Agent  ###############################
 resource "helm_release" "opa" {
-  name              = var.helm_release_name
-  repository        = "https://spartan-stratos.github.io/helm-charts/"
-  chart             = "spartan"
-  version           = var.helm_chart_version
-  namespace         = var.namespace
-  create_namespace  = true
+  name             = var.helm_release_name
+  repository       = "https://spartan-stratos.github.io/helm-charts/"
+  chart            = "spartan"
+  version          = var.helm_chart_version
+  namespace        = var.namespace
+  create_namespace = true
 
   set {
-    name = "image.repository"
+    name  = "image.repository"
     value = "openpolicyagent/opa"
   }
 
   set {
-    name = "image.tag"
+    name  = "image.tag"
     value = var.opa_image_tag
   }
 
   set {
-    name = "containerPort"
+    name  = "containerPort"
     value = 8181
   }
 
