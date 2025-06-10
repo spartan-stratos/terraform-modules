5f0d968a1cd201a9c54e9bb3a2cfb2699f59d075
spartan-ytruong <y.truong@c0x12c.com>
2025-06-10 15:08:00 +0700

    add `cloudfront_distribution_arn`

--- aws/static-website/modules/cloudfront/outputs.tf
+++ aws/static-website/modules/cloudfront/outputs.tf
@@ -6,3 +6,8 @@ output "cloudfront_id" {
   description = "The Cloudfront ID that hosting the web"
   value       = aws_cloudfront_distribution.this.id
 }
+
+output "cloudfront_distribution_arn" {
+  description = "Cloudfront distribution arn."
+  value       = aws_cloudfront_distribution.this.arn
+}
