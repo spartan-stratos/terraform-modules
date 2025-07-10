152a4a52707e42ab44e8e7d9d51278895296c780
spartan-tainguyen <tai.nguyen@c0x12c.com>
2025-07-10 10:14:19 +0700

    update dependencies

--- aws/jenkins-oidc/main.tf
+++ aws/jenkins-oidc/main.tf
@@ -1,6 +1,6 @@
 module "provider" {
   source  = "c0x12c/oidc-provider/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 
   url                    = var.url
   client_id_list         = var.client_id_list
