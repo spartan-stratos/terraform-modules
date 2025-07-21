e7dca9d189f27cf6248ca73b4c06f5f33854175a
spartan-quannguyen <quan.nguyen@c0x12c.com>
2025-07-21 11:56:38 +0700

    update docs in main.tf

--- gcp/load-balancer-gcs/main.tf
+++ gcp/load-balancer-gcs/main.tf
@@ -1,4 +1,14 @@
+/**
+This `google_client_config` data source retrieves the current authenticated user's Google Cloud configuration.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
+ */
+
 data "google_client_config" "this" {}
+
+/**
+This google_compute_global_address resource creates a global static IP address for the load balancer.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_global_address
+*/
 
 resource "google_compute_global_address" "this" {
   project      = data.google_client_config.this.project
@@ -6,6 +16,11 @@ resource "google_compute_global_address" "this" {
   ip_version   = "IPV4"
   address_type = "EXTERNAL"
 }
+
+/**
+This google_compute_backend_bucket resource configures a backend bucket for serving content from a Google Cloud Storage bucket.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_backend_bucket
+*/
 
 resource "google_compute_backend_bucket" "this" {
   provider = google
@@ -15,6 +30,11 @@ resource "google_compute_backend_bucket" "this" {
   bucket_name = var.bucket_name
   enable_cdn  = var.enable_cdn
 }
+
+/**
+This google_compute_url_map resource defines how requests are routed to the backend bucket.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_url_map
+*/
 
 resource "google_compute_url_map" "bucket" {
   provider = google
@@ -26,12 +46,22 @@ resource "google_compute_url_map" "bucket" {
   default_service = google_compute_backend_bucket.this.self_link
 }
 
+/**
+This google_compute_target_http_proxy resources create HTTP proxy for the load balancer.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_target_http_proxy
+*/
+
 resource "google_compute_target_http_proxy" "http" {
   count   = var.enable_http ? 1 : 0
   project = data.google_client_config.this.project
   name    = "${var.prefix_name}-http-proxy"
   url_map = google_compute_url_map.bucket.id
 }
+
+/**
+This google_compute_global_forwarding_rule resource creates a global forwarding rule for HTTP traffic.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_global_forwarding_rule
+*/
 
 resource "google_compute_global_forwarding_rule" "http" {
   count                 = var.enable_http ? 1 : 0
@@ -44,6 +74,11 @@ resource "google_compute_global_forwarding_rule" "http" {
   load_balancing_scheme = "EXTERNAL_MANAGED"
 }
 
+/**
+This google_compute_managed_ssl_certificate resource creates a managed SSL certificate for the load balancer.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_managed_ssl_certificate
+*/
+
 resource "google_compute_managed_ssl_certificate" "this" {
   count = var.enable_ssl ? 1 : 0
 
@@ -53,6 +88,11 @@ resource "google_compute_managed_ssl_certificate" "this" {
   }
 }
 
+/**
+This google_compute_target_https_proxy resource creates an HTTPS proxy for the load balancer.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_target_https_proxy
+*/
+
 resource "google_compute_target_https_proxy" "https" {
   count   = var.enable_ssl ? 1 : 0
   project = data.google_client_config.this.project
@@ -61,6 +101,11 @@ resource "google_compute_target_https_proxy" "https" {
 
   certificate_map = var.certificate_map
 }
+
+/**
+This google_compute_global_forwarding_rule resource creates a global forwarding rule for HTTPS traffic.
+https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_global_forwarding_rule
+*/
 
 resource "google_compute_global_forwarding_rule" "https" {
   count                 = var.enable_ssl ? 1 : 0
